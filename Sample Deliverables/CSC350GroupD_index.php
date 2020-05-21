92c46c4260bb803b8f079d1f5a24c058065fe43a
Shirley Ni <24558017+holysmokes48@users.noreply.github.com>
2020-05-21 15:43:12 -0400

    FINAL SOURCE CODE

---
+++ Sample Deliverables/CSC350GroupD_index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+	<title>BMCC Scheduling Program</title>
+	<style>
+			h1
+			{
+				text-align: center;
+				font-size: 40px;
+			}
+			
+			.Submitbtn
+			{
+				font-size: 32px;
+				font-family: "Times New Roman";
+				text-align: center;
+				height:150px;
+				width:650px;
+				float:left;
+				margin: 0 auto; 
+				
+			}
+			
+			p.paragraph
+			{
+				font-size: 32px;
+				font-family: "Times New Roman";
+				text-align: center;
+				text-decoration: underline;
+			}
+			
+	</style>
+</head>
+
+<h1><a href= "CSC350GroupD_Index.php">BMCC Scheduling Program </a></h1>
+<body>
+<p class = "paragraph"> Please Choose From the following: </p>
+	<form action="CSC350GroupD_Index.php" method="post">
+		<tr>
+				<tr>
+					<td colspan = "2">&nbsp;
+					<a href= "CSC350GroupD_scheduling.php">
+					<input type="button" value="Schedule Classes" name="ScheduleClasses" class = "Submitbtn"/></a></td>
+				</tr>
+				
+				<tr>
+					<td colspan = "2">&nbsp;
+					<a href= "CSC350GroupD_aboutpage.php">
+					<input type="button" value="About Page" name="AboutPage" class = "Submitbtn"/></a></td>
+				</tr>
+			</tr>
+		<table>
+		
+	</form>
+
+
+<?php
+
+?>
+
+
+</body>
+</html>
